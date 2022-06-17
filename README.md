63eb77b1c6d372c9051fd17721b1b459610e25ea
Andrew Burgess <aburgess@redhat.com>
2022-06-17 11:24:50 +0100

    Rewrite script to scrape books from the website

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # bOOkp
 
+## Background
+
+Originally forked from: https://github.com/moyix/bOOkp/fork
+
+The original script tried to download the books directly.  But by the
+time I started looking at the script this wasn't working any more.
+
+So, I hacked up the script to scrape the Amazon website and download
+the books as a user would, this worked fine at the time I initially
+wrote this script, but it probably going to break as the site changes
+over time.
+
+## Original Description
+
 Quick'n'dirty script to download all you Kindle ebooks.
 
 I needed to backup all my Kindle e-books, so put together this script. It does
@@ -23,7 +37,7 @@ to. This is important, because the downloaded books will be DRMd to that
 particular device. The serial number (which is required to remove DRM) will be
 printed when the books are downloaded.
 
-## Usage
+### Usage
 
 ```
 usage: bookp.py [-h] [--verbose] [--showbrowser] --email EMAIL
@@ -44,6 +58,41 @@ optional arguments:
   --asin [ASIN [ASIN ...]]
                         list of ASINs to download
 ```
+
+## New Description
+
+The script is now works in a multi-stage process.  First usage will be
+something like this:
+
+  `bookp.py --email=EMAIL --showbrowser --verbose`
+
+This will start up a browser window, sign in to your Amazon account,
+and switch to the books page.
+
+Then the script will start downloading all the books on the first page.
+
+Once the first page is complete the script will exit.
+
+At this point you should manually select the second page of books, and rerun the script like this:
+
+  `bookp.py --email=EMAIL --url=URL --sessionid=ID --showbrowser --verbose`
+
+The URL and ID will hav been printed the first time the script ran.
+This second invocation of the script will reconnect to the existing
+browser session and download all the books on the second page.
+
+After which the script exits again.
+
+Select the third page of books, and repeat, etc.
+
+## NOTES
+
+The function `find_device_radio_element` is written to suite my needs.
+I have two devices, and I want to download the books for the second
+device.
+
+You will likely need to update this function to suite the number of
+devices that you have.
 
 ## Requirements
 
--- bookp.py
+++ bookp.py
@@ -8,6 +8,7 @@ import re
 import requests
 import sys
 import urllib.parse
+import time
 
 from argparse import ArgumentParser
 from pyvirtualdisplay import Display
@@ -32,8 +33,8 @@ def create_session(email, password, browser_visible=False, proxy=None):
         options.add_argument('--proxy-server='+proxy)
     browser = webdriver.Chrome(chrome_options=options)
 
-    logger.info("Loading www.amazon.com")
-    browser.get('https://www.amazon.com')
+    logger.info("Loading www.amazon.co.uk")
+    browser.get('https://www.amazon.co.uk')
 
     logger.info("Logging in")
     hover = browser.find_element_by_css_selector('#nav-link-accountList > div')
@@ -57,114 +58,92 @@ def create_session(email, password, browser_visible=False, proxy=None):
         input("Press Enter to continue...")
     except TimeoutException:
         pass
-    logger.info("Getting CSRF token")
-    browser.get('https://www.amazon.com/hz/mycd/myx#/home/content/booksAll')
 
-    match = re.search('var csrfToken = "(.*)";', browser.page_source)
-    if match:
-        csrf_token = match.group(1)
+    logger.info("Switching to books page")
+    browser.get('https://www.amazon.co.uk/hz/mycd/myx#/home/content/booksAll')
+    browser.implicitly_wait(4)
 
-    cookies = {}
-    for cookie in browser.get_cookies():
-        cookies[cookie['name']] = cookie['value']
+    return browser
 
+def reconnect_session(url, session_id):
+    logger.info("Reconnecting to browser")
+    browser = webdriver.Remote(command_executor=url, desired_capabilities={})
+    browser.close()   # this prevents the dummy browser
+    browser.session_id = session_id
+
+    return browser
+
+def find_confirm_element(browser):
+    for confirm in browser.find_elements_by_xpath("//*[starts-with(@id,'DOWNLOAD_AND_TRANSFER_ACTION_') and contains(@id,'_CONFIRM')]/span"):
+        if confirm.is_displayed():
+            return confirm
+
+    raise Exception("Failed to find Confirm button")
+
+def find_notification_close_element(browser):
+    for e in browser.find_elements_by_id('notification-close'):
+        if e.is_displayed():
+            return e
+
+    raise Exception("Failed to find notification close button")
+
+def click_download(browser):
+    confirm = find_confirm_element(browser)
+    webdriver.ActionChains(browser).move_to_element(confirm).click().perform()
+    browser.implicitly_wait(3)
+
+    logger.info("clicking the notification close button")
+    n_close = find_notification_close_element(browser)
+    webdriver.ActionChains(browser).move_to_element(n_close).click().perform()
+    browser.implicitly_wait(1)
+
+    logger.info("ready to download the next book")
+
+def find_download_element(browser):
+    for span in browser.find_elements_by_xpath("//span[contains(text(),'Download & transfer via USB')]"):
+        if span.is_displayed():
+            return span
+
+    raise Exception("Download & transfer element not found")
+
+def find_device_radio_element(browser):
+    for list in browser.find_elements_by_xpath("//*[contains(@id, 'download_and_transfer_list_')]"):
+        if list.is_displayed():
+            return list.find_element_by_xpath("li[2]/div[1]/label/input")
+
+    return None
+
+def download_books(browser):
+    logger.info("Downloading all books on page")
+    skip_count = 0
+    for element in browser.find_elements_by_class_name("dropdown_title"):
+        element.click()
+        browser.implicitly_wait(1)
+
+        logger.info("Finding Download & transfer button")
+        btn = find_download_element(browser)
+        logger.info("Clicking Download & transfer button")
+        btn.click()
+        browser.implicitly_wait(1)
+
+        logger.info("Select device radio button")
+        radio = find_device_radio_element(browser)
+        if radio is None:
+            print("!! Skipping book, no device radio button found")
+            skip_count += 1
+            continue
+        radio.click()
+
+        logger.info("Downloading book")
+        click_download(browser)
+
+    if skip_count > 0:
+        print(f"Skipped {skip_count} books due to missing device support")
+
+def shutdown_session(browser, browser_visible=False):
     browser.quit()
     if not browser_visible:
         display.stop();
-
-    return cookies, csrf_token
-
-"""
-NOTE: This function is not used currently, because the download URL can be
-constructed without this additional request. This might change in the future,
-so I'm keeping this here just in case.
-
-def get_download_url(user_agent, cookies, csrf_token, asin, device_id):
-    logger.info("Getting download URL for " + asin)
-    data_json = {
-        'param':{
-            'DownloadViaUSB':{
-                'contentName':asin,
-                'encryptedDeviceAccountId':device_id, # device['deviceAccountId']
-                'originType':'Purchase'
-            }
-        }
-    }    
-
-    r = requests.post('https://www.amazon.com/hz/mycd/ajax',
-        data={'data':json.dumps(data_json), 'csrfToken':csrf_token},
-        headers=user_agent, cookies=cookies)
-    rr = json.loads(r.text)["DownloadViaUSB"]
-    return rr["URL"] if rr["success"] else None
-"""
-
-def get_devices(user_agent, cookies, csrf_token):
-    logger.info("Getting device list")
-    data_json = {'param': {'GetDevices': {}}}
-    
-    r = requests.post('https://www.amazon.com/hz/mycd/ajax',
-        data={'data':json.dumps(data_json), 'csrfToken':csrf_token},
-        headers=user_agent, cookies=cookies)
-    devices = json.loads(r.text)["GetDevices"]["devices"]
-
-    return [device for device in devices if 'deviceSerialNumber' in device]
-
-def get_asins(user_agent, cookies, csrf_token):
-    logger.info("Getting e-book list")
-    startIndex = 0
-    batchSize = 100
-    data_json = {
-        'param':{
-            'OwnershipData':{
-                'sortOrder':'DESCENDING',
-                'sortIndex':'DATE',
-                'startIndex':startIndex,
-                'batchSize':batchSize,
-                'contentType':'Ebook',
-                'itemStatus':['Active'],
-                'originType':['Purchase'],
-            }
-        }
-    }
-
-    # NOTE: This loop could be replaced with only one request, since the
-    # response tells us how many items are there ('numberOfItems'). I guess that
-    # number will never be high enough to cause problems, but I want to be on
-    # the safe side, hence the download in batches approach.
-    asins = []
-    while True:
-        r = requests.post('https://www.amazon.com/hz/mycd/ajax',
-            data={'data':json.dumps(data_json), 'csrfToken':csrf_token},
-            headers=user_agent, cookies=cookies)
-        rr = json.loads(r.text)
-        asins += [book['asin'] for book in rr['OwnershipData']['items']]
-
-        if rr['OwnershipData']['hasMoreItems']:
-            startIndex += batchSize
-            data_json['param']['OwnershipData']['startIndex'] = startIndex
-        else:
-            break
-
-    return asins
-
-def download_books(user_agent, cookies, device, asins, directory):
-    logger.info("Downloading {} books".format(len(asins)))
-    cdn_url = 'http://cde-g7g.amazon.com/FionaCDEServiceEngine/FSDownloadContent'
-    cdn_params = 'type=EBOK&key={}&fsn={}&device_type={}'
-    for asin in asins:
-        try:
-            params = cdn_params.format(asin, device['deviceSerialNumber'], device['deviceType'])
-            r = requests.get(cdn_url, params=params, headers=user_agent, cookies=cookies, stream=True)
-            name = re.findall("filename\*=UTF-8''(.+)", r.headers['Content-Disposition'])[0]
-            name = urllib.parse.unquote(name)
-            name = name.replace('/', '_')
-            with open(os.path.join(directory, name), 'wb') as f:
-                for chunk in r.iter_content(chunk_size=512):
-                    f.write(chunk)
-            logger.info('Downloaded ' + asin + ': ' + name)
-        except Exception as e:
-            logger.debug(e)
-            logger.error('Failed to download ' + asin)
 
 def main():
     parser = ArgumentParser(description="Amazon e-book downloader.")
@@ -172,55 +151,43 @@ def main():
     parser.add_argument("--showbrowser", help="display browser while creating session.", action="store_true")
     parser.add_argument("--email", help="Amazon account e-mail address", required=True)
     parser.add_argument("--password", help="Amazon account password", default=None)
-    parser.add_argument("--outputdir", help="download directory (default: books)", default="books")
     parser.add_argument("--proxy", help="HTTP proxy server", default=None)
-    parser.add_argument("--asin", help="list of ASINs to download", nargs='*')
+    parser.add_argument("--url", help="URL for existing browser session", default=None)
+    parser.add_argument("--sessionid", help="Session ID for existing browser session", default=None)
     args = parser.parse_args()
 
     if args.verbose:
         logger.setLevel(logging.INFO)
     else:
         logger.setLevel(logging.WARNING)
+
     formatter = logging.Formatter('[%(levelname)s]\t%(asctime)s %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    password = args.password
-    if not password:
-        password = getpass.getpass("Your Amazon password: ")
+    if not args.url:
+        password = args.password
+        if not password:
+            password = getpass.getpass("Your Amazon password: ")
 
-    if os.path.isfile(args.outputdir):
-        logger.error("Output directory is a file!")
-        return -1
-    elif not os.path.isdir(args.outputdir):
-        os.mkdir(args.outputdir)
+        browser = create_session(args.email, password,
+                                 browser_visible=args.showbrowser,
+                                 proxy=args.proxy)
 
-    cookies, csrf_token = create_session(args.email, password,
-        browser_visible=args.showbrowser, proxy=args.proxy)
-    if not args.asin:
-        asins = get_asins(user_agent, cookies, csrf_token)
+        url = browser.command_executor._url
+        session_id = browser.session_id
+
+        print(f"url: {url}")
+        print(f"session-id: {session_id}")
     else:
-        asins = args.asin
+        if not args.sessionid:
+            print("missing --sessionid argument")
+            exit(1)
 
-    devices = get_devices(user_agent, cookies, csrf_token)
-    print("Please choose which device you want to download your e-books to!")
-    for i in range(len(devices)):
-        print(" " + str(i) + ". " + devices[i]['deviceAccountName'])
-    while True:
-        try:
-            choice = int(input("Device #: "))
-        except:
-            logger.error("Not a number!")
-        if choice in range(len(devices)):
-            break
+        browser = reconnect_session(args.url, args.sessionid)
 
-    download_books(user_agent, cookies, devices[choice], asins, args.outputdir)
-
-    print("\n\nAll done!\nNow you can use apprenticeharper's DeDRM tools " \
-            "(https://github.com/apprenticeharper/DeDRM_tools)\n" \
-            "with the following serial number to remove DRM: " +
-            devices[choice]['deviceSerialNumber'])
+    download_books(browser)
 
 if __name__ == '__main__':
     try:
